464b4088f67193218331f047206d1224892fa003
januarr <aldojanuar40@gmail.com>
2020-08-10 09:54:44 +0700

    projek rental mobil

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('about')
+<br>
+<br>
+
+<div class="card mb-3"style="width: 1200px">
+    <img src="{{asset('assets/images/assasin.jpg')}}" style="height: 300px"class="card-img-top" alt="...">
+    <div class="card-body">
+      <h1 class="card-title">About Perusahaan</h1>
+      <p class="card-text">Projek web ini dibuat untuk memenuhi perbaikan nilai mata kuliah framework programing, web ini berbasis di bidang perentalan yaitu Rental Mobil
+          dengan menghubungkan antara penyedia jasa rental dengan pelanggan dan untuk mempermudah antara penyedia jasa rental dan pelanggan untuk menyewa
+          mobil.
+      </p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+@endsection
+
